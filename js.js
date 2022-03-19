87dcdee7dabc02d44d6798db0b57b14da629031d
gombi <gombos1998@gmail.com>
2022-03-19 12:56:45 +0100

    modosítás1.1

--- js.js
+++ js.js
@@ -14,17 +14,46 @@ var kepek = [
         cim: "Jelző kő",
         eleresiut:"./kepek/03.jpg",
         leírás:"Fagyott jelző kő a kékes tetején",
+    },
+    {
+        cim: "Jelző kő",
+        eleresiut:"./kepek/04.jpg",
+        leírás:"Fagyott jelző kő a kékes tetején",
+    },
+    {
+        cim: "Jelző kő",
+        eleresiut:"./kepek/05.jpg",
+        leírás:"Fagyott jelző kő a kékes tetején",
+    },
+    {
+        cim: "Jelző kő",
+        eleresiut:"./kepek/06.jpg",
+        leírás:"Fagyott jelző kő a kékes tetején",
     }
+
 ]
 function ID(elem){
     return document.getElementById(elem); //rövidítő elem
+}
+function CLASS(elem)
+{
+    return document.getElementsByClassName(elem)
+}
+function $(elem)
+{
+ return document.querySelectorAll(elem)
 }
 function init(){
     kiirKepek()
     ID("galeria").style.backgroundColor="pink";
     //ID("galeria").style.border="12px solid pink";
     for (let index = 0; index < kepek.length; index++) {
-        document.getElementsByClassName("kepek")[index].style.border="5px solid grey";
+       /* CLASS("kepek")[index].style.border="12px solid grey";
+        CLASS("kepek")[index].style.padding="10px"; // ez még lefut
+        $('#galeria div img')[index].style.backgroundColor='purple'; // itt szétesik
+        */
+       $("#galeria div")[index].className="divekFormazasa" //osztályt hozunk létre
+
     }
     
 
