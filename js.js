6c760476e9ad533fe8a88ee0acd5327dc50abe20
gombi <gombos1998@gmail.com>
2022-03-19 12:34:09 +0100

    alfa

--- js.js
+++ js.js
@@ -1,5 +1,5 @@
-window.addEventListener("load", init);
-kepek = [
+window.addEventListener("load", init); //betölti az oldalt , majd az init függvényt hozzá rendeli
+var kepek = [
     {
         cim: "Téli panoráma kép",
         eleresiut:"./kepek/01.jpg",
@@ -17,15 +17,22 @@ kepek = [
     }
 ]
 function ID(elem){
-    return document.getElementById(elem);
+    return document.getElementById(elem); //rövidítő elem
 }
 function init(){
     kiirKepek()
+    ID("galeria").style.backgroundColor="pink";
+    //ID("galeria").style.border="12px solid pink";
+    for (let index = 0; index < kepek.length; index++) {
+        document.getElementsByClassName("kepek")[index].style.border="5px solid grey";
+    }
+    
+
 }
 function kiirKepek(){
     var txt = " ";
     for (let i = 0; i < kepek.length; i++) {
-        txt = txt + "<div>" + "<h3>"+ kepek[i].cim +"</h3>" + "<img src='"+ kepek[i].eleresiut+ "' class='kepek' alt='cicás képek'/>"+
+        txt = txt + "<div>" + "<h3>"+ kepek[i].cim +"</h3>" + "<img src='"+ kepek[i].eleresiut+ "' class='kepek' alt='Panoráma képek'/>"+
         "<p>"+kepek[i].leírás+"</p> </div>"
     }
     ID("galeria").innerHTML = txt;
